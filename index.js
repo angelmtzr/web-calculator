0f5553ec56f3f3575d3240709405d2b030f589e3
Ángel Martínez <angel.mtz.r02@gmail.com>
2021-10-01 18:11:10 -0500

    Toggle themes was implemented and some bugs were fixed

--- index.js
+++ index.js
@@ -2,33 +2,37 @@ var operation = ["0"];
 var result;
 $(".btn-n,.btn-s,.btn-f").on("click",function(){
     var btn = $(this).attr("id");
-    console.log(btn);
     if(btn === "reset"){
         $("#screen-text").text("0");
-        operation = [];
-        operation.push("0");
+        operation = ["0"];
     }
     else if(btn === "del"){
         console.log(operation.join(""));
         if(operation.length === 1){
-            console.log("yes");
-            $("#screen-text").text("0");
+            console.log("long 1");
+            operation = ["0"];
+            $("#screen-text").text(operation.join(""));
         }
         else{
             operation.pop();
-            $("#screen-text").text("0");
+            console.log("poped, now "+operation.join(""));
+            $("#screen-text").text(operation.join(""));
         }
-        
-        
     }
     else if(btn === "equals"){
         result = eval(operation.join(""));
         $("#screen-text").text(result);
+        operation = ["0"];
     }
     else{
-        console.log("lol");
+        console.log("clicked");
         if (operation.join("") === "0") operation.pop();
         operation.push(btn);
         $("#screen-text").text(operation.join(""));
     }
+});
+$(".slider").on("click", function(){
+    var theme = this.value;
+    $("#theme-style").attr("href","style"+theme+".css");
+    console.log($("#theme-style").attr("href"));
 });
